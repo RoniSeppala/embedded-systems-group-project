7401b94ebd53b345b362d45133394368f4fd0d88
Roni Seppälä <roni.o.seppala@gmail.com>
2026-05-04 02:36:47 +0300

    added i2c activators for slave activity

--- ATmega328p/main.c
+++ ATmega328p/main.c
@@ -10,7 +10,10 @@
 
 // mega output handling
 #include "outputs.h"
+#include "delay.h"
 
+
+#define UNO_OUTPUT_UPDATE_DELAY_MS (25u)
 
 // TODO: add error handling
 
@@ -19,24 +22,31 @@ static void elevator_handle_command(uint8_t command)
     switch (command)
     {
         case ELEVATOR_CMD_ALL_OFF:
+            outputs_all_off();
             break;
 
         case ELEVATOR_CMD_MOVING:
+            outputs_set_movement();
             break;
 
         case ELEVATOR_CMD_DOOR_OPENING:
+            outputs_set_door_opening();
             break;
 
         case ELEVATOR_CMD_DOOR_CLOSING:
+            outputs_set_door_closing();
             break;
 
         case ELEVATOR_CMD_OBSTACLE:
+            outputs_set_obstacle();
             break;
 
         case ELEVATOR_CMD_BUZZER_STOP:
+            outputs_stop_buzzer();
             break;
 
         default:
+            outputs_all_off();
             break;
     }
 }
@@ -57,6 +67,9 @@ int main(void)
         {
             elevator_handle_command(command);
         }
+
+        outputs_update();
+        DELAY_ms(UNO_OUTPUT_UPDATE_DELAY_MS);
     }
  
     return 0;
