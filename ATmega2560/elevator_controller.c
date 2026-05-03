5fc0819fe75f4c30d3401a994509bc6fd5aaaaa6
Roni Seppälä <roni.o.seppala@gmail.com>
2026-05-04 01:24:57 +0300

    added most of supporting elevator fuctions

--- ATmega2560/elevator_controller.c
+++ ATmega2560/elevator_controller.c
@@ -14,6 +14,10 @@
 #include "i2c_master.h"
 
 // constants and types
+#define ELEVATOR_MAX_FLOOR (99u)
+#define ELEVATOR_INPUT_MAX_DIGITS (2u)
+#define KEYPAD_CONFIRM_KEY '#'
+#define KEYPAD_CLEAR_KEY '*'
 #define ELEVATOR_INITIAL_FLOOR (0u)
 
 typedef enum
@@ -32,14 +36,123 @@ static elevator_state_t current_state = ELEVATOR_STATE_IDLE;
 static uint8_t current_floor = ELEVATOR_INITIAL_FLOOR;
 static uint8_t target_floor = ELEVATOR_INITIAL_FLOOR;
 
-// lcd idle helper
+// helper functions
+static uint8_t is_digit_key(uint8_t key)
+{
+    return ((key >= '0') && (key <= '9'));
+}
+
+static void elevator_display_current_floor(void)
+{
+    lcd_gotoxy(0, 1);
+    lcd_puts("Current: ");
+
+    lcd_putc((current_floor / 10u) + '0');
+    lcd_putc((current_floor % 10u) + '0');
+    lcd_puts("      ");
+}
+
 static void elevator_display_idle(void)
 {
     lcd_clrscr();
     lcd_gotoxy(0, 0);
     lcd_puts("Choose floor");
+    elevator_display_current_floor();
+}
+
+static void elevator_display_input(uint8_t input_value, uint8_t digit_count)
+{
+    lcd_clrscr();
+    lcd_gotoxy(0, 0);
+    lcd_puts("Floor: ");
+
+    if (digit_count == 0u)
+    {
+        lcd_puts("__");
+    }
+    else if (digit_count == 1u)
+    {
+        lcd_putc(input_value + '0');
+    }
+    else
+    {
+        lcd_putc((input_value / 10u) + '0');
+        lcd_putc((input_value % 10u) + '0');
+    }
+
     lcd_gotoxy(0, 1);
-    lcd_puts("Current: 0");
+    lcd_puts("#=OK *=Clear");
+}
+
+static void elevator_set_fault_same_floor(void)
+{
+    current_state = ELEVATOR_STATE_FAULT;
+
+    lcd_clrscr();
+    lcd_gotoxy(0, 0);
+    lcd_puts("Same floor");
+}
+
+static void elevator_select_target_floor(uint8_t selected_floor)
+{
+    target_floor = selected_floor;
+
+    if (target_floor == current_floor)
+    {
+        elevator_set_fault_same_floor();
+    }
+    else if (target_floor > current_floor)
+    {
+        current_state = ELEVATOR_STATE_GOING_UP;
+    }
+    else
+    {
+        current_state = ELEVATOR_STATE_GOING_DOWN;
+    }
+}
+
+static void elevator_read_floor_input(void)
+{
+    uint8_t key;
+    uint8_t input_value = 0u;
+    uint8_t digit_count = 0u;
+
+    elevator_display_input(input_value, digit_count);
+
+    while (current_state == ELEVATOR_STATE_IDLE)
+    {
+        key = KEYPAD_GetKey();
+
+        if (is_digit_key(key))
+        {
+            if (digit_count < ELEVATOR_INPUT_MAX_DIGITS)
+            {
+                input_value = (input_value * 10u) + (key - '0');
+                digit_count++;
+
+                if (input_value > ELEVATOR_MAX_FLOOR)
+                {
+                    input_value = 0u;
+                    digit_count = 0u;
+                }
+
+                elevator_display_input(input_value, digit_count);
+            }
+        }
+        else if (key == KEYPAD_CLEAR_KEY)
+        {
+            input_value = 0u;
+            digit_count = 0u;
+            elevator_display_input(input_value, digit_count);
+        }
+        else if (key == KEYPAD_CONFIRM_KEY)
+        {
+            if (digit_count > 0u)
+            {
+                elevator_select_target_floor(input_value);
+            }
+        }
+    }
 }
 
 // elevator code
@@ -61,4 +174,8 @@ void elevator_controller_init(void)
 // elevator logic
 void elevator_controller_run(void)
 {
+    if (current_state == ELEVATOR_STATE_IDLE)
+    {
+        elevator_read_floor_input();
+    }
 }
